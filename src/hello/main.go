6465e605526c4367934c7bf88f9532a672606f05
Bill Barbour <bill.barbour@oracle.com>
2016-04-25 14:39:54 -0400

    Using the error pkg

--- src/hello/main.go
+++ src/hello/main.go
@@ -3,11 +3,14 @@ package main
 import (
     "fmt"
     "os"
+    "errors"
 )
+
+var ( errorEmptyString = errors.New("Unwilling to print an empty string") )
 
 func printer(msg string) error {
   if msg == "" {
-    return fmt.Errorf("Unwilling to print an empty string")
+    return errorEmptyString
   }
   _, err := fmt.Println("%s\n", msg)
   return err
@@ -15,8 +18,13 @@ func printer(msg string) error {
 
 func main() {
   if err := printer(""); err != nil {
-    fmt.Printf("printer failed: %s\n",err)
+    if err == errorEmptyString {
+      fmt.Printf("You tried to print an empty string\n")
+    } else {
+      fmt.Printf("printer failed: %s\n",err)
+    }
     os.Exit(1)
+
   }
 
 }
