e6651ef781f6e4bb762d6755c96b9aa0d8532576
Bill Barbour <bill.barbour@oracle.com>
2016-05-24 08:29:33 -0400

    Using sqlx package

--- src/model1Sql/main.go
+++ src/model1Sql/main.go
@@ -1,54 +1,29 @@
 package main
 
 import (
-  "database/sql"
+  "github.com/jmoiron/sqlx"
 _  "github.com/lib/pq"
   "time"
   "log"
 )
 
 type Todo struct {
-  id int
-  subject string
-  dueDate time.Time
-  isComplete bool
+  Id int
+  Subject string
+  DueDate time.Time
+  IsComplete bool
 }
 
 func main() {
-  db, err := sql.Open("postgres", "postgres://postgres:postgres@localhost/playground?sslmode=disable")
+  db, err := sqlx.Open("postgres", "postgres://postgres:postgres@localhost/playground?sslmode=disable")
   if err != nil {
     log.Fatal(err)
   }
 
-  var max int
-  idRow := db.QueryRow("select max(id) from todos")
-  err = idRow.Scan(&max)
-  if err != nil {
-    log.Fatal(err)
-  }
+  todos := []Todo{}
+  db.Select(&todos, "select id, subject, due_date AS DueDate, is_complete AS IsComplete from todos")
 
-  now := time.Now()
-  res, err := db.Exec("insert into todos (id, subject, due_date, is_complete) values ($1, $2, $3, $4)", max + 1, "Todo from Go", now, "false")
-
-  if err  != nil {
-    log.Fatal(err)
-  }
-
-  affected, _ := res.RowsAffected()
-  log.Printf("Rows affected %d\n", affected)
-
-  rows, err := db.Query("select * from todos")
-  if err != nil {
-    log.Fatal(err)
-  }
-
-  for rows.Next() {
-    todo := Todo{}
-
-    err = rows.Scan(&todo.id, &todo.subject, &todo.dueDate, &todo.isComplete)
-    if err != nil {
-      log.Fatal(err)
-    }
-    log.Printf("Id %d: subject is %s\n", todo.id, todo.subject)
+  for _, todo := range todos {
+    log.Printf("Id %d: subject is %s\n", todo.Id, todo.Subject)
   }
 }
