8381f8cfc547e009463f9fe589a46a896188ef83
Bill Barbour <bill.barbour@oracle.com>
2016-05-24 08:47:53 -0400

    Using db transactions

--- src/model1Sql/main.go
+++ src/model1Sql/main.go
@@ -20,6 +20,22 @@ func main() {
     log.Fatal(err)
   }
 
+  tx := db.MustBegin() // Begin transaction
+  var currId int
+  currIdRow := db.QueryRow("select max(id) from todos")
+  err = currIdRow.Scan(&currId)
+  now := time.Now()
+
+  t := Todo {
+    Id: currId + 1,
+    Subject: "Mow Lawn",
+    DueDate: now,
+    IsComplete: false,
+  }
+
+  tx.Exec("insert into todos (id, subject, due_date, is_complete) values ($1, $2, $3, $4)", t.Id, t.Subject, t.DueDate, t.IsComplete)
+  tx.Commit() //End transaction
+
   todos := []Todo{}
   db.Select(&todos, "select * from todos")
 
