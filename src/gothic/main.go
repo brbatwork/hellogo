ac1f6a1dc238292653b1d67b916de792da96d387
Bill Barbour <bill.barbour@oracle.com>
2016-06-16 15:29:21 -0400

    Using gothic for shorter code

--- src/gothic/main.go
+++ src/gothic/main.go
@@ -10,6 +10,7 @@ import (
   "github.com/markbates/goth"
   "github.com/markbates/goth/providers/twitter"
   "github.com/markbates/goth/providers/facebook"
+  "github.com/markbates/goth/gothic"
   "github.com/gorilla/sessions"
 )
 
@@ -32,40 +33,46 @@ func main() {
     r.HTML(resp, http.StatusOK, "index", nil)
   })
 
-  p.Get("/auth/{provider}", func(resp http.ResponseWriter, req *http.Request) {
-    providerName := req.URL.Query().Get(":provider")
-    log.Println("Handling a request to provider: ", providerName)
-    provider, err := goth.GetProvider(providerName)
-    if err != nil {
-      fmt.Fprint(resp, err)
-      return
-    }
+  // p.Get("/auth/{provider}", func(resp http.ResponseWriter, req *http.Request) {
+  //   providerName := req.URL.Query().Get(":provider")
+  //   log.Println("Handling a request to provider: ", providerName)
+  //   provider, err := goth.GetProvider(providerName)
+  //   if err != nil {
+  //     fmt.Fprint(resp, err)
+  //     return
+  //   }
 
-    sess, err := provider.BeginAuth("play_state")
-    if err != nil {
-      fmt.Fprint(resp, err)
-      return
-    }
+  //   sess, err := provider.BeginAuth("play_state")
+  //   if err != nil {
+  //     fmt.Fprint(resp, err)
+  //     return
+  //   }
 
-    url, err := sess.GetAuthURL()
-    if err != nil {
-      fmt.Fprint(resp, err)
-      return
-    }
+  //   url, err := sess.GetAuthURL()
+  //   if err != nil {
+  //     fmt.Fprint(resp, err)
+  //     return
+  //   }
 
-    session, _ := Store.Get(req, "goth-session")
-    session.Values["goth-session"] = sess.Marshal()
-    err = session.Save(req, resp)
-    if err != nil {
-      fmt.Fprint(resp, err)
-      return
-    }
+  //   session, _ := Store.Get(req, "goth-session")
+  //   session.Values["goth-session"] = sess.Marshal()
+  //   err = session.Save(req, resp)
 
-    http.Redirect(resp, req, url, http.StatusFound)
-  })
+
+  //   http.Redirect(resp, req, url, http.StatusFound)
+  // })
+
+  p.Get("/auth/{provider}", gothic.BeginAuthHandler)
 
   p.Get("/auth/{provider}/callback", func(resp http.ResponseWriter, req *http.Request) {
-    fmt.Fprintf(resp, "Hello!")
+    user, err := gothic.CompleteUserAuth(resp, req)
+    if err != nil {
+      fmt.Fprint(resp, err)
+      log.Println(err)
+      return
+    }
+
+    r.HTML(resp, http.StatusOK, "user", user)
   })
 
   http.ListenAndServe(":4000", p)
