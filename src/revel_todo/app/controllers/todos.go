cf30057b212aa2f3fe42a4744ffc832fa08c53f4
Bill Barbour <bill.barbour@oracle.com>
2016-06-22 10:23:36 -0400

    Added Edit/New routes to todo

--- src/revel_todo/app/controllers/todos.go
+++ src/revel_todo/app/controllers/todos.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
   "github.com/revel/revel"
+  "fmt"
   "revel_todo/app/models"
 )
 
@@ -22,4 +23,35 @@ func (c Todos) Show(id int) revel.Result {
   }
 
   return c.Render(t)
+}
+
+func (c Todos) New() revel.Result {
+  return c.Render(&models.Todo{})
+}
+
+func (c Todos) Edit(id int) revel.Result {
+  t, err := models.Get(id)
+  if err != nil {
+    return c.RenderError(err)
+  }
+  return c.Render(t)
+}
+
+func (c Todos) Update(id int, t *models.Todo) revel.Result {
+  c.Validation.Required(t.Subject)
+  if c.Validation.HasErrors() {
+    c.RenderArgs["t"] = t
+    return c.RenderTemplate("Todos/Edit.html")
+  }
+
+  et, err := models.Get(id)
+  if err != nil {
+    return c.RenderError(err)
+  }
+
+  et.Subject = t.Subject
+  et.Description = t.Description
+  et.Update()
+  c.Flash.Success("Your todo was successfully updated")
+  return c.Redirect(fmt.Sprintf("/todos/%d", t.ID))
 }
